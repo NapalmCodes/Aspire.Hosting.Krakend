2160dd30227f507944449b67008b7f9a903aa1e2
napalm684 <svause@gmail.com>
2024-08-24 21:52:24 -0400

    Fix build.

--- build/GetBuildVersion.sh
+++ build/GetBuildVersion.sh
@@ -1,31 +1,23 @@
-function GetBuildVersion() {
-    VersionString="$1"
+#!/usr/bin/env bash
 
-    # Use regex to match the version components
-    if [[ "$VersionString" =~ ^([0-9]+)(\.([0-9]+))?(\.([0-9]+))?(\-([0-9A-Za-z\-\.]+))?(\+([0-9]+))?$ ]]; then
-        Major="${BASH_REMATCH[1]}"
-        Minor="${BASH_REMATCH[3]}"
-        Patch="${BASH_REMATCH[5]}"
-        PreReleaseTag="${BASH_REMATCH[7]}"
-        BuildRevision="${BASH_REMATCH[9]}"
+VersionString=$1
 
-        # Handle empty values
-        [[ -z "$Minor" ]] && Minor="0"
-        [[ -z "$Patch" ]] && Patch="0"
-        [[ -z "$BuildRevision" ]] && BuildRevision="0"
+# Use regex to match the version components
+if [[ $VersionString =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)? ]]; then
+    Major="${BASH_REMATCH[1]}"
+    Minor="${BASH_REMATCH[2]}"
+    Patch="${BASH_REMATCH[3]}"
+    tags=(${VersionString//-/ })
+    PreReleaseTag=${tags[1]}
 
-        Version="${Major}.${Minor}.${Patch}"
-        
-        if [[ -n "$PreReleaseTag" ]]; then
-            Version="${Version}-${PreReleaseTag}"
-        fi
+    Version="${Major}.${Minor}.${Patch}"
 
-        if [[ "$BuildRevision" -ne 0 ]]; then
-            Version="${Version}.${BuildRevision}"
-        fi
-
-        echo "$Version"
-    else
-        echo "1.0.0-build"
+    if [[ -n "$PreReleaseTag" ]]; then
+        Version="${Version}-${PreReleaseTag}"
     fi
-}
+
+    echo "$Version"
+else
+    echo "Error: Version string '$VersionString' does not match the required format." >&2
+    exit 1
+fi
